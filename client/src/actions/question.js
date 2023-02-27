d92d8938743ca30ae3ef0c841981f0f039484268
Kanak Dubey <126403840+im-KanaK@users.noreply.github.com>
2023-02-27 20:11:33 +0530

    Update question.js

--- client/src/actions/question.js
+++ client/src/actions/question.js
@@ -1,4 +1,4 @@
-import * as  api from '../api'
+import * as api from '../api/index'
 
 export const askQuestion = (questionData, navigate) => async (dispatch) => {
     try {
@@ -13,7 +13,6 @@ export const askQuestion = (questionData, navigate) => async (dispatch) => {
 
 export const fetchAllQuestions = () => async (disptach) => {
     try {
-        
         const { data } = await api.getAllQuestions()
         disptach({ type: 'FETCH_ALL_QUESTIONS', payload: data})
     } catch (error) {
@@ -21,20 +20,9 @@ export const fetchAllQuestions = () => async (disptach) => {
     }
 }
 
-export const postAnswer = (answerData) => async (dispatch) => {
-    try {
-        const { id, noOfAnswers, answerBody, userAnswered, userId } = answerData;
-        const { data } = await api.postAnswer( id, noOfAnswers, answerBody, userAnswered, userId )
-        dispatch({ type: 'POST_ANSWER', payload: data})
-        dispatch(fetchAllQuestions())
-    } catch (error) {
-        console.log(error)
-    }
-}
-
 export const deleteQuestion = (id, navigate) => async (dispatch) => {
     try {
-        const { data }= api.deleteQuestion(id)
+        await api.deleteQuestion(id)
         dispatch(fetchAllQuestions())
         navigate('/')
     } catch (error) {
@@ -42,22 +30,31 @@ export const deleteQuestion = (id, navigate) => async (dispatch) => {
     }
 }
 
-export const deleteAnswer = (id, answerId, noOfAnswers) => async (dispatch) => {
+export const voteQuestion = (id, value) => async (dispatch) => {
     try {
-        const {data} = await api.deleteAnswer(id, answerId, noOfAnswers)
-        dispatch(fetchAllQuestions())
-    } catch (error) {
-        console.log(error)
-    }
-} 
-
-
-
-export const voteQuestion = (id, value,userId) => async (dispatch) => {
-    try {
-        const { data }=await api.voteQuestion(id, value, userId)
+        await api.voteQuestion(id, value)
         dispatch(fetchAllQuestions())
     } catch (error) {
         console.log(error)
     }
 }
+
+export const postAnswer = (answerData) => async (dispatch) => {
+    try {
+        const { id, noOfAnswers, answerBody, userAnswered } = answerData;
+        const { data } = await api.postAnswer( id, noOfAnswers, answerBody, userAnswered )
+        dispatch({ type: 'POST_ANSWER', payload: data})
+        dispatch(fetchAllQuestions())
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const deleteAnswer = (id, answerId, noOfAnswers) => async (dispatch) => {
+    try {
+        await api.deleteAnswer(id, answerId, noOfAnswers)
+        dispatch(fetchAllQuestions())
+    } catch (error) {
+        console.log(error)
+    }
+} 
