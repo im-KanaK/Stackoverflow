83ac08112810a88c38e90fa433d4c339d129c7ee
Kanak Dubey <126403840+im-KanaK@users.noreply.github.com>
2023-03-02 12:13:24 +0530

    Create Chat.js

---
+++ client/src/Chat.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+class KommunicateChat extends Component {
+   constructor(props){
+       super(props);
+   } 
+componentDidMount(){
+   (function(d, m){
+       var kommunicateSettings =
+           {"appId":"1a62a263c386b79583b12cb9408d3cc1b","popupWidget":true,"automaticChatOpenOnNavigation":true};
+       var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+       s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+       var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+       window.kommunicate = m; m._globals = kommunicateSettings;
+   })(document, window.kommunicate || {});
+}
+render(){
+   return(
+       <div>
+           <h1>Hello</h1>
+     </div>
+   )
+}
+}
+export default KommunicateChat;
