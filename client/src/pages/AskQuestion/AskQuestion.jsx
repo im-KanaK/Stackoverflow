8196acb85b5c2ca7039c014dff0281c3c19ae5ee
Kanak Dubey <126403840+im-KanaK@users.noreply.github.com>
2023-03-03 18:32:40 +0530

    Update AskQuestion.jsx

--- client/src/pages/AskQuestion/AskQuestion.jsx
+++ client/src/pages/AskQuestion/AskQuestion.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {useNavigate } from "react-router-dom";
-import "./AskQuestion.css";
+import "./AskQuestion.css";// eslint-disable-next-line
 import { askQuestion } from "../../actions/question";
 
 const AskQuestion = () => {
